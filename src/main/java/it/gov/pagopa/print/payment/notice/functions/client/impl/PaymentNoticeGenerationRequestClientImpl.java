f959d777c249bb9b8718978d403cfe0d231e517d
Alessio Cialini <alessio.cialini@emeal.nttdata.com>
2024-07-05 11:33:32 +0200

    rework mongo client

--- src/main/java/it/gov/pagopa/print/payment/notice/functions/client/impl/PaymentNoticeGenerationRequestClientImpl.java
+++ src/main/java/it/gov/pagopa/print/payment/notice/functions/client/impl/PaymentNoticeGenerationRequestClientImpl.java
@@ -8,8 +8,6 @@ import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Updates;
 import it.gov.pagopa.print.payment.notice.functions.client.PaymentNoticeGenerationRequestClient;
 import it.gov.pagopa.print.payment.notice.functions.entity.PaymentNoticeGenerationRequest;
-import it.gov.pagopa.print.payment.notice.functions.entity.PaymentNoticeGenerationRequestError;
-import org.bson.codecs.configuration.CodecProvider;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
 
@@ -23,44 +21,34 @@ public class PaymentNoticeGenerationRequestClientImpl implements PaymentNoticeGe
 
     private static PaymentNoticeGenerationRequestClientImpl instance;
 
-    private static MongoClient mongoClient;
     private static final CodecRegistry pojoCodecRegistry = fromRegistries(getDefaultCodecRegistry(),
             fromProviders(PojoCodecProvider.builder().automatic(true).build()));
 
-    private static void createClient() {
+    private static MongoClient mongoClient;
+
+    private static MongoClient createClient() {
         String connectionString = System.getenv("NOTICE_REQUEST_MONGODB_CONN_STRING");
-        mongoClient = MongoClients.create(connectionString);
+        return mongoClient != null ? mongoClient : MongoClients.create(connectionString);
     }
 
-    public MongoCollection<PaymentNoticeGenerationRequest> getMongoCollection() {
+    public MongoCollection<PaymentNoticeGenerationRequest> getMongoCollection(MongoClient mongoClient) {
         String databaseName = System.getenv("NOTICE_REQUEST_MONGO_DB_NAME");
         String collectionName = System.getenv("NOTICE_REQUEST_MONGO_COLLECTION_NAME");
-        if (mongoClient == null) {
-            createClient();
-        }
         try {
-            MongoDatabase database;
-        try {
-            database = mongoClient.getDatabase(databaseName)
+            MongoDatabase database = mongoClient.getDatabase(databaseName)
                     .withCodecRegistry(pojoCodecRegistry);
+            return database.getCollection(
+                    collectionName, PaymentNoticeGenerationRequest.class);
         } catch (Exception e) {
             mongoClient.close();
-            throw e;
-        }
-            return database.getCollection(collectionName, PaymentNoticeGenerationRequest.class);
-        } catch (Exception e) {
-            mongoClient.close();
-            mongoClient = null;
             throw new RuntimeException("Error recovering db", e);
         }
     }
 
-    PaymentNoticeGenerationRequestClientImpl() {
-        createClient();
-    }
+    PaymentNoticeGenerationRequestClientImpl() {}
 
     PaymentNoticeGenerationRequestClientImpl(MongoClient mongoClient) {
-        this.mongoClient = mongoClient;
+        PaymentNoticeGenerationRequestClientImpl.mongoClient = mongoClient;
     }
 
     public static PaymentNoticeGenerationRequestClientImpl getInstance() {
@@ -73,13 +61,17 @@ public class PaymentNoticeGenerationRequestClientImpl implements PaymentNoticeGe
 
     @Override
     public Optional<PaymentNoticeGenerationRequest> findById(String folderId) {
-        return Optional.ofNullable(getMongoCollection().find(Filters.eq("_id", folderId)).first());
+        try (MongoClient mongoClient = createClient()) {
+            return Optional.ofNullable(getMongoCollection(mongoClient).find(Filters.eq("_id", folderId)).first());
+        }
     }
 
     @Override
     public void updatePaymentGenerationRequest(PaymentNoticeGenerationRequest paymentNoticeGenerationRequest) {
-        getMongoCollection().updateOne(Filters.eq("_id", paymentNoticeGenerationRequest.getId()),
-                Updates.set("status", paymentNoticeGenerationRequest.getStatus().name()));
+        try (MongoClient mongoClient = createClient()) {
+            getMongoCollection(mongoClient).updateOne(Filters.eq("_id", paymentNoticeGenerationRequest.getId()),
+                    Updates.set("status", paymentNoticeGenerationRequest.getStatus().name()));
+        }
     }
 
 }
